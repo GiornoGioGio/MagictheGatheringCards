be6b6634b7fb551b09dd2e765dee78a4dc6d0ce1
GiornoGioGio <mikolaj.jedras@gmail.com>
2019-09-18 16:26:38 +0200

    Simple app using Magic the Gathering: API

--- app/src/main/java/com/example/android/magicthegatheringcards/MainActivity.java
+++ app/src/main/java/com/example/android/magicthegatheringcards/MainActivity.java
@@ -2,19 +2,43 @@ package com.example.android.magicthegatheringcards;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.util.JsonReader;
+import java.util.ArrayList;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
 
-public class MainActivity extends AppCompatActivity{
+import javax.net.ssl.HttpsURLConnection;
+import android.os.AsyncTask;
+
+public class MainActivity extends AsyncTask<Void, Void, String[]>{
+    ArrayList<String> value;
+    String key;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        String[] data;
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        final TextView result = findViewById(R.id.network);
-        GetCards getCards = new GetCards();
-        getCards.key = "type";
+    protected String[] doInBackground(Void... arg0) {
+        try {
+            URL source = new URL("https://api.magicthegathering.io/v1/cards");
+            HttpsURLConnection connection = (HttpsURLConnection) source.openConnection();
+            InputStream responseBody = connection.getInputStream();
+            InputStreamReader responseBodyReader =
+                    new InputStreamReader(responseBody, "UTF-8");
+            JsonReader jsonReader = new JsonReader(responseBodyReader);
+            jsonReader.beginObject();
+            while (jsonReader.hasNext()) {
+                key = jsonReader.nextName();
+                if (key.equals("organization_url")) {
+                    value.add(jsonReader.nextString());
+                    break;
+                } else {
+                    jsonReader.skipValue();
+                }
+            }
+        } catch (Exception e) {
+            return null;
+        }
+        return null;
     }
 
 }
