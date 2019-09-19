8749333917ffed3d8e4d5fb3efe685ee9c0144e8
GiornoGioGio <mikolaj.jedras@gmail.com>
2019-09-19 17:53:46 +0200

    Simple app using Magic the Gathering: API

---
+++ app/src/main/java/com/example/android/magicthegatheringcards/GetCards.java
@@ -0,0 +1,61 @@
+package com.example.android.magicthegatheringcards;
+
+import android.util.JsonReader;
+import java.util.ArrayList;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+import android.os.AsyncTask;
+import android.widget.TextView;
+
+public class GetCards extends AsyncTask<Void, Void, Void>{
+    ArrayList<String> value;
+    String key;
+    TextView result;
+    String network;
+
+    protected GetCards(String keyC, ArrayList<String> valueC, TextView resultC){
+        key = keyC;
+        value = valueC;
+        result = resultC;
+    }
+
+    @Override
+    protected Void doInBackground(Void... arg0) {
+        try {
+            URL source = new URL("https://api.magicthegathering.io/v1/cards");
+            HttpsURLConnection connection = (HttpsURLConnection) source.openConnection();
+            InputStream responseBody = connection.getInputStream();
+            InputStreamReader responseBodyReader =
+                    new InputStreamReader(responseBody, "UTF-8");
+            JsonReader jsonReader = new JsonReader(responseBodyReader);
+            jsonReader.beginObject();
+            while (jsonReader.hasNext()) {
+                key = jsonReader.nextName();
+                if (key.equals("organization_url")) {
+                    network =
+                            result.getText().toString() + value.add(jsonReader.nextString()) + "\n";
+                    break;
+                } else {
+                    jsonReader.skipValue();
+                }
+            }
+        } catch (Exception e) {
+            return null;
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(){
+        super.onPostExecute(aVoid);
+        if(isCancelled()){
+            return;
+        }
+        result.setText(network);
+    }
+
+}
--- app/src/main/java/com/example/android/magicthegatheringcards/MainActivity.java
+++ app/src/main/java/com/example/android/magicthegatheringcards/MainActivity.java
@@ -2,51 +2,18 @@ package com.example.android.magicthegatheringcards;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.util.JsonReader;
-import java.util.ArrayList;
-
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
-
-import javax.net.ssl.HttpsURLConnection;
-import android.os.AsyncTask;
 import android.widget.TextView;
 
-public class MainActivity extends AsyncTask<Void, Void, Void>{
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
     ArrayList<String> value;
-    String key;
 
-    @Override
-    protected void onPreExecute(){
-        TextView result;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView result = findViewById(R.id.textView);
+        GetCards getCards = new GetCards("Sorcery", value, result);
+        getCards.execute();
     }
-
-    @Override
-    protected Void doInBackground(Void... arg0) {
-        try {
-            URL source = new URL("https://api.magicthegathering.io/v1/cards");
-            HttpsURLConnection connection = (HttpsURLConnection) source.openConnection();
-            InputStream responseBody = connection.getInputStream();
-            InputStreamReader responseBodyReader =
-                    new InputStreamReader(responseBody, "UTF-8");
-            JsonReader jsonReader = new JsonReader(responseBodyReader);
-            jsonReader.beginObject();
-            while (jsonReader.hasNext()) {
-                key = jsonReader.nextName();
-                if (key.equals("organization_url")) {
-                    value.add(jsonReader.nextString());
-                    break;
-                } else {
-                    jsonReader.skipValue();
-                }
-            }
-        } catch (Exception e) {
-            return null;
-        }
-        return null;
-    }
-
 }
